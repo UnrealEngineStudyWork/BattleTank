2ad14d4f3ef90d0c6113a74446bf7693c8556a40
DESKTOP-2NSGSF6\gamer <gamerz1497@live.com>
2017-02-16 14:12:31 +0200

    S4L98 Creating and deliting landscapes

---
+++ BattleTankBattles/Source/BattleTankBattles/BattleTankBattlesGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTankBattles.h"
+#include "BattleTankBattlesGameModeBase.h"
+
+
+
+
---
+++ BattleTankBattles/Source/BattleTankBattles/BattleTankBattlesGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankBattlesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKBATTLES_API ABattleTankBattlesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
